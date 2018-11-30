3cb687d22ac0ff1faeb64014c69e53d981e74edb
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2018-11-30 01:21:23 -0500

    Display message if there is no total Income

--- src/routes/Summary.js
+++ src/routes/Summary.js
@@ -4,16 +4,7 @@ import { Button, Card, Tile, Header, Icon, List } from "react-native-elements"
 import TestGraph from "../components/TestGraph"
 import gql from "graphql-tag"
 import { Query } from "react-apollo"
-import {
-	PRIMARY_COLOR,
-	SECONDARY_COLOR,
-	TERTIARY,
-	QUATERNARY,
-	QUINARY,
-	LIGHT_GREEN,
-	CARD_BACKGROUND,
-	BACKGROUND
-} from "../AppTheme"
+import { TERTIARY, LIGHT_GREEN, CARD_BACKGROUND, BACKGROUND } from "../AppTheme"
 
 import DefaultHeader from "../components/DefaultHeader"
 
@@ -41,7 +32,7 @@ export default class Summary extends React.Component {
 					<Card
 						title="Monthly Income"
 						containerStyle={{
-							height: 175,
+							height: 195,
 							backgroundColor: CARD_BACKGROUND,
 							borderColor: "transparent",
 							borderRadius: 25,
@@ -52,36 +43,71 @@ export default class Summary extends React.Component {
 
 						//wrapperStyle={{ display: "flex", justifyContent: "center", alignItems: "center" }}
 					>
-						<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
-							<View
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									justifyContent: "center",
-									alignItems: "center"
-								}}
-							>
-								<Icon
-									type="font-awesome"
-									name="plus"
-									color={LIGHT_GREEN}
-									style={{ paddingRighteft: 50 }}
-									onPress={() => this.props.navigation.navigate("Income")}
-								/>
-								<Text style={{ color: LIGHT_GREEN }}>Add Income</Text>
-							</View>
+						<Query query={totalIncome}>
+							{({ loading, data }) => {
+								if (loading) {
+									return null
+								}
+								if (data.totalIncome === null) {
+									return (
+										<View
+											style={{
+												display: "flex",
+												flexDirection: "column",
+												justifyContent: "center",
+												alignItems: "center",
+												width: "100%"
+											}}
+										>
+											<Text style={{ color: "white", fontSize: 25 }}>
+												Press{"   "}
+												<Icon
+													type="font-awesome"
+													name="plus"
+													color={LIGHT_GREEN}
+													style={{ paddingRighteft: 50 }}
+													onPress={() => this.props.navigation.navigate("Income")}
+												/>
+												{"   "}
+												to Add an Income
+											</Text>
+										</View>
+									)
+								}
 
-							<Query query={totalIncome}>
-								{({ loading, data }) => {
-									if (loading) {
-										return null
-									}
-									return <Text style={{ color: "white", fontSize: 50 }}>${data.totalIncome}</Text>
-								}}
-							</Query>
-
-							<View />
-						</View>
+								return (
+									<View
+										style={{
+											display: "flex",
+											flexDirection: "column",
+											justifyContent: "center",
+											alignItems: "center"
+										}}
+									>
+										<View style={{ paddingBottom: 20 }}>
+											<Text style={{ color: "white", fontSize: 40 }}>${data.totalIncome}</Text>
+										</View>
+										<View
+											style={{
+												display: "flex",
+												flexDirection: "column",
+												justifyContent: "center",
+												alignItems: "center"
+											}}
+										>
+											<Icon
+												type="font-awesome"
+												name="plus"
+												color={LIGHT_GREEN}
+												style={{ paddingRighteft: 50 }}
+												onPress={() => this.props.navigation.navigate("Income")}
+											/>
+											<Text style={{ color: LIGHT_GREEN }}>Add Income</Text>
+										</View>
+									</View>
+								)
+							}}
+						</Query>
 					</Card>
 					<Card
 						title="Monthly Expenses"
@@ -115,7 +141,7 @@ export default class Summary extends React.Component {
 								/>
 								<Text style={{ color: TERTIARY }}>Add Income</Text>
 							</View>
-							<Text style={{ color: "white", fontSize: 50 }}>$0</Text>
+							<Text style={{ color: "white", fontSize: 40 }}>$0</Text>
 							<View />
 						</View>
 					</Card>
