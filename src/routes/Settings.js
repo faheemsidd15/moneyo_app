f90720b9761399f30a5be2114b94f12516ef8908
faheemsidd15 <blazefs@gmail.com>
2019-05-06 19:37:31 -0400

    scrolling bar

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -1,23 +1,91 @@
-import React from "react"
-import { View, Text, AsyncStorage, ScrollView } from "react-native"
-import { Button, Card, Tile, Header, Icon, List } from "react-native-elements"
-import DefaultHeader from "../components/DefaultHeader"
-import { BACKGROUND } from "../AppTheme"
+import React, { Component } from "react"
+import { Animated, View, StyleSheet, Image, Dimensions, ScrollView } from "react-native"
 
-export default class Settings extends React.Component {
+const deviceWidth = Dimensions.get("window").width
+const FIXED_BAR_WIDTH = 280
+const BAR_SPACE = 10
+
+const cards = ["income", "expenses"]
+
+export default class App extends Component {
+	numItems = cards.length
+	itemWidth = FIXED_BAR_WIDTH / this.numItems - (this.numItems - 1) * BAR_SPACE
+	animVal = new Animated.Value(0)
+
 	render() {
+		let barArray = []
+		cards.forEach((image, i) => {
+			const scrollBarVal = this.animVal.interpolate({
+				inputRange: [deviceWidth * (i - 1), deviceWidth * (i + 1)],
+				outputRange: [-this.itemWidth, this.itemWidth],
+				extrapolate: "clamp"
+			})
+
+			const thisBar = (
+				<View
+					key={`bar${i}`}
+					style={[
+						styles.track,
+						{
+							width: this.itemWidth,
+							marginLeft: i === 0 ? 0 : BAR_SPACE
+						}
+					]}
+				>
+					<Animated.View
+						style={[
+							styles.bar,
+							{
+								width: this.itemWidth,
+								transform: [{ translateX: scrollBarVal }]
+							}
+						]}
+					/>
+				</View>
+			)
+			barArray.push(thisBar)
+		})
+
 		return (
-			<View>
-				<DefaultHeader showMenu={true} open={this.props.navigation.openDrawer} title="Settings" />
-				<ScrollView style={{ backgroundColor: BACKGROUND, height: "100%" }}>
-					<Card title="Summary">
-						<View>
-							<Text>Hello</Text>
-							<Text>From Settings</Text>
-						</View>
-					</Card>
+			<View style={styles.container} flex={1}>
+				<ScrollView
+					horizontal
+					showsHorizontalScrollIndicator={false}
+					scrollEventThrottle={10}
+					pagingEnabled
+					onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: this.animVal } } }])}
+				>
+					<View style={{ width: deviceWidth - 50, backgroundColor: "purple", margin: 5, height: 100 }}>Hello</View>
+					<View style={{ width: deviceWidth - 50, backgroundColor: "purple", margin: 5, height: 100 }}>Hello</View>
 				</ScrollView>
+				<View style={styles.barContainer}>{barArray}</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	barContainer: {
+		position: "absolute",
+		zIndex: 2,
+		top: 400,
+		flexDirection: "row"
+	},
+	track: {
+		backgroundColor: "#ccc",
+		overflow: "hidden",
+		height: 2
+	},
+	bar: {
+		backgroundColor: "#5294d6",
+		height: 2,
+		position: "absolute",
+		left: 0,
+		top: 0
+	}
+})
--- src/routes/Summary.js
+++ src/routes/Summary.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { View, Text, AsyncStorage, ScrollView } from "react-native"
 import { Button, Card, Tile, Header, Icon, List } from "react-native-elements"
-import TestGraph from "../components/TestGraph"
-import TestPie from "../components/TestPieChart"
+import { View, Text, AsyncStorage, ScrollView, Animated, Dimensions, StyleSheet } from "react-native"
 
 import { TOTAL_MONTHLY_INCOME } from "../Queries"
 import { Query } from "react-apollo"
@@ -11,148 +9,223 @@ import { TERTIARY, LIGHT_GREEN, CARD_BACKGROUND, BACKGROUND, QUATERNARY, QUINARY
 import DefaultHeader from "../components/DefaultHeader"
 import { LinearGradient } from "expo"
 
+const deviceWidth = Dimensions.get("window").width
+const deviceHeight = Dimensions.get("window").height
+const FIXED_BAR_WIDTH = 300
+const BAR_SPACE = 10
+
+const cards = ["income", "expenses"]
+
 class Summary extends React.Component {
-  render() {
-    const { navigation, data } = this.props
+	numItems = cards.length
+	itemWidth = FIXED_BAR_WIDTH / this.numItems - (this.numItems - 1) * BAR_SPACE
+	animVal = new Animated.Value(0)
 
-    if (navigation.state.routeName === "Summary") {
-      data.refetch(TOTAL_MONTHLY_INCOME)
-    }
+	render() {
+		let barArray = []
+		cards.forEach((image, i) => {
+			const scrollBarVal = this.animVal.interpolate({
+				inputRange: [deviceWidth * (i - 1), deviceWidth * (i + 1)],
+				outputRange: [-this.itemWidth, this.itemWidth],
+				extrapolate: "clamp"
+			})
 
-    return (
-      <View style={{ height: "100%" }}>
-        <DefaultHeader showMenu={true} open={this.props.navigation.openDrawer} title="MoneyO" />
+			const thisBar = (
+				<View
+					key={`bar${i}`}
+					style={[
+						styles.track,
+						{
+							width: this.itemWidth,
+							marginLeft: i === 0 ? 0 : BAR_SPACE
+						}
+					]}
+				>
+					<Animated.View
+						style={[
+							styles.bar,
+							{
+								width: this.itemWidth,
+								transform: [{ translateX: scrollBarVal }]
+							}
+						]}
+					/>
+				</View>
+			)
+			barArray.push(thisBar)
+		})
 
-        {/* <View
-          style={{
-            height: 175,
-            backgroundColor: "black"
-          }}
-        >
-           <TestGraph /> 
-          <TestPie />
-        </View> */}
-        <LinearGradient colors={[BACKGROUND, TERTIARY]} style={{ height: "100%" }}>
-          <ScrollView>
-            <Card
-              title="Monthly Income"
-              containerStyle={{
-                height: 195,
-                backgroundColor: CARD_BACKGROUND,
-                borderColor: "transparent",
-                borderRadius: 25,
-                overflow: "hidden"
-              }}
-              titleStyle={{ fontSize: 20, color: LIGHT_GREEN, textAlign: "center" }}
-              dividerStyle={{ backgroundColor: LIGHT_GREEN }}
+		const { navigation, data } = this.props
 
-              //wrapperStyle={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-            >
-              <Query query={TOTAL_MONTHLY_INCOME} notifyOnNetworkStatusChange={true}>
-                {({ loading, data }) => {
-                  if (loading) {
-                    return null
-                  }
-                  if (data.totalMonthlyIncome === null) {
-                    return (
-                      <View
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          justifyContent: "center",
-                          alignItems: "center",
-                          width: "100%"
-                        }}
-                      >
-                        <Text style={{ color: "white", fontSize: 25 }}>
+		if (navigation.state.routeName === "Summary") {
+			data.refetch(TOTAL_MONTHLY_INCOME)
+		}
+
+		return (
+			<View style={{ height: "100%" }}>
+				<DefaultHeader showMenu={true} open={this.props.navigation.openDrawer} title="MoneyO" />
+
+				<LinearGradient colors={[BACKGROUND, TERTIARY]} style={{ height: deviceHeight, width: deviceWidth }}>
+					<View style={{ width: "100%", height: 100 }}>
+						<Text>Summary</Text>
+					</View>
+
+					<ScrollView
+						horizontal
+						showsHorizontalScrollIndicator={false}
+						scrollEventThrottle={10}
+						pagingEnabled
+						onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: this.animVal } } }])}
+					>
+						<Card
+							title="Monthly Income"
+							containerStyle={{
+								height: 500,
+								width: 300,
+								backgroundColor: CARD_BACKGROUND,
+								borderColor: "transparent",
+								borderRadius: 10,
+								overflow: "hidden"
+							}}
+							titleStyle={{ fontSize: 20, color: LIGHT_GREEN, textAlign: "left" }}
+							dividerStyle={{ backgroundColor: LIGHT_GREEN }}
+
+							//wrapperStyle={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+						>
+							<Query query={TOTAL_MONTHLY_INCOME} notifyOnNetworkStatusChange={true}>
+								{({ loading, data }) => {
+									if (loading) {
+										return null
+									}
+									if (data.totalMonthlyIncome === null) {
+										return (
+											<View
+												style={{
+													display: "flex",
+													flexDirection: "column",
+													justifyContent: "center",
+													alignItems: "center",
+													width: "100%"
+												}}
+											>
+												<Text style={{ color: "white", fontSize: 25 }}>
 													Press{"   "}
-                          <Icon
-                            type="font-awesome"
-                            name="plus"
-                            color={LIGHT_GREEN}
-                            onPress={() => this.props.navigation.navigate("Income")}
-                          />
-                          {"   "}
+													<Icon
+														type="font-awesome"
+														name="plus"
+														color={LIGHT_GREEN}
+														onPress={() => this.props.navigation.navigate("Income")}
+													/>
+													{"   "}
 													to Add an Income
-                        </Text>
-                      </View>
-                    )
-                  }
+												</Text>
+											</View>
+										)
+									}
 
-                  return (
-                    <View
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center"
-                      }}
-                    >
-                      <View style={{ paddingBottom: 20 }}>
-                        <Text style={{ color: "black", fontSize: 40 }}>${data.totalMonthlyIncome}</Text>
-                      </View>
-                      <View
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          justifyContent: "center",
-                          alignItems: "center"
-                        }}
-                      >
-                        <Icon
-                          type="font-awesome"
-                          name="plus"
-                          color={LIGHT_GREEN}
-                          size={45}
-                          onPress={() => this.props.navigation.navigate("Income")}
-                        />
-                      </View>
-                    </View>
-                  )
-                }}
-              </Query>
-            </Card>
-            <Card
-              title="Monthly Expenses"
-              containerStyle={{
-                height: 175,
-                backgroundColor: CARD_BACKGROUND,
-                borderColor: "transparent",
-                borderRadius: 25,
-                overflow: "hidden"
-              }}
-              titleStyle={{ fontSize: 20, color: TERTIARY, textAlign: "center" }}
-              dividerStyle={{ backgroundColor: TERTIARY }}
+									return (
+										<View
+											style={{
+												display: "flex",
+												flexDirection: "column",
+												justifyContent: "center",
+												alignItems: "center"
+											}}
+										>
+											<View style={{ paddingBottom: 20 }}>
+												<Text style={{ color: "black", fontSize: 40 }}>${data.totalMonthlyIncome}</Text>
+											</View>
+											<View
+												style={{
+													display: "flex",
+													flexDirection: "column",
+													justifyContent: "center",
+													alignItems: "center"
+												}}
+											>
+												<Icon
+													type="font-awesome"
+													name="plus"
+													color={LIGHT_GREEN}
+													size={45}
+													onPress={() => this.props.navigation.navigate("Income")}
+												/>
+											</View>
+										</View>
+									)
+								}}
+							</Query>
+						</Card>
+						<Card
+							title="Monthly Expenses"
+							containerStyle={{
+								height: 500,
+								width: 300,
+								backgroundColor: CARD_BACKGROUND,
+								borderColor: "transparent",
+								borderRadius: 10,
+								overflow: "hidden"
+							}}
+							titleStyle={{ fontSize: 20, color: TERTIARY, textAlign: "center" }}
+							dividerStyle={{ backgroundColor: TERTIARY }}
 
-              //wrapperStyle={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-            >
-              <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
-                <View
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Icon
-                    type="font-awesome"
-                    name="plus"
-                    color={TERTIARY}
-                    style={{ paddingRighteft: 50 }}
-                    onPress={() => this.props.navigation.navigate("Income")}
-                  />
-                  <Text style={{ color: TERTIARY }}>Add Income</Text>
-                </View>
-                <Text style={{ color: "white", fontSize: 40 }}>$0</Text>
-                <View />
-              </View>
-            </Card>
-          </ScrollView>
-        </LinearGradient>
-      </View>
-    )
-  }
+							//wrapperStyle={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+						>
+							<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+								<View
+									style={{
+										display: "flex",
+										flexDirection: "column",
+										justifyContent: "center",
+										alignItems: "center"
+									}}
+								>
+									<Icon
+										type="font-awesome"
+										name="plus"
+										color={TERTIARY}
+										style={{ paddingRighteft: 50 }}
+										onPress={() => this.props.navigation.navigate("Income")}
+									/>
+									<Text style={{ color: TERTIARY }}>Add Income</Text>
+								</View>
+								<Text style={{ color: "white", fontSize: 40 }}>$0</Text>
+								<View />
+							</View>
+						</Card>
+					</ScrollView>
+					<View style={styles.barContainer}>{barArray}</View>
+				</LinearGradient>
+			</View>
+		)
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	barContainer: {
+		position: "absolute",
+		zIndex: 2,
+		bottom: 150,
+		left: 50,
+		flexDirection: "row"
+	},
+	track: {
+		backgroundColor: "white",
+		overflow: "hidden",
+		height: 2
+	},
+	bar: {
+		backgroundColor: BACKGROUND,
+		height: 2,
+		position: "absolute",
+		left: 0,
+		top: 0
+	}
+})
 
 export default graphql(TOTAL_MONTHLY_INCOME)(Summary)
